b0f4c9138df6be9fddfbb50d948df7b643c519a7
Akshay Mithari <akshaymithari@Akshays-MacBook-Pro.local>
2023-05-13 14:36:31 +0530

    comitment

--- src/approval/approval.jsx
+++ src/approval/approval.jsx
@@ -1,6 +1,7 @@
 import {React,useState,useEffect} from 'react'
 import Navbar from '../components/NavbarComp'
 import SubNavbar from '../components/subNavbar'
+import swal from "sweetalert";
 import Dean from '../Dean/dean'
 import Badge from "react-bootstrap/Badge";
 import Hod from '../Hod/hod'
@@ -18,9 +19,16 @@ const Approval = () => {
   const [aict1,setAict1] = useState(true);
   const [newBtnShow1,setnewBtnShow1] = useState(false);
   const [newBtnShow, setnewBtnShow] = useState(true);
+
   const [conhod,setconhod]=useState(false);
   const [projecthod, projectsethod] = useState([]);
-  
+
+  const [condean,setcondean]=useState(false);
+  const [projectdean, projectsetdean] = useState([]);
+
+  const [conaict,setconaict]=useState(false);
+  const [projectaict, projectsetaict] = useState([]);
+
   const handleGuid = () => {
     setguid(true);
     setguid1(false);
@@ -64,7 +72,7 @@ const Approval = () => {
     const role=localStorage.getItem('role');
       
     if(role==="Hod"){
-      setcon(true);
+      setconhod(true);
       const userId=localStorage.getItem("userId")
       fetch("http://localhost:8000/project/")
         .then((response) => response.json())
@@ -77,10 +85,10 @@ const Approval = () => {
       fetch("http://localhost:8000/project/")
         .then((response) => response.json())
         .then((data) =>{
-          
+          data=data.filter((e)=>e.hod!=null)
         console.log(data);
         projectsethod(data)});
-        setconhod(true);
+       
     }
   };
 
@@ -103,6 +111,30 @@ const Approval = () => {
     setguid(false);
     setAict(true);
     setAict1(false);
+
+        ///////start dean display
+        const role=localStorage.getItem('role');
+      
+        if(role==="Dean"){
+          setcondean(true);
+          const userId=localStorage.getItem("userId")
+          fetch("http://localhost:8000/project/")
+            .then((response) => response.json())
+            .then((data) =>{
+            data=data.filter((e)=>e.dean==userId)
+            projectsetdean(data)});
+            
+        }else {
+          
+          const userId=localStorage.getItem("userId")
+          fetch("http://localhost:8000/project/")
+            .then((response) => response.json())
+            .then((data) =>{
+             data=data.filter((e)=>e.dean!=null)
+            console.log(data);
+            projectsetdean(data)});
+            
+        }
   };
 
   const handleAict = () => {
@@ -116,6 +148,32 @@ const Approval = () => {
     setguid(false);
     setDean(true);
     setDean1(false);
+
+            ///////start dean display
+            const role=localStorage.getItem('role');
+      
+            if(role==="Aicte member"){
+              setcondean(true);
+              const userId=localStorage.getItem("userId")
+              fetch("http://localhost:8000/project/")
+                .then((response) => response.json())
+                .then((data) =>{
+                data=data.filter((e)=>e.aicte==userId)
+                projectsetaict(data)});
+                
+            }else {
+              
+              const userId=localStorage.getItem("userId")
+              fetch("http://localhost:8000/project/")
+                .then((response) => response.json())
+                .then((data) =>{
+                 data=data.filter((e)=>e.aicte!=null)
+                console.log(data);
+                projectsetaict(data)});
+                
+            }
+
+
   };
   const handleAict1 = () => {
     setAict1(false);
@@ -123,8 +181,129 @@ const Approval = () => {
     setguid(true);
     setguid1(false);
   };
+  const college =localStorage.getItem("college");
+  const approveAicte=(e)=>{
 
+  }
+  const disapprovedaict=(e)=>{
+
+    var requestOptions = {
+      method: 'DELETE',
+      redirect: 'follow'
+    };
     
+    fetch("http://localhost:8000/project/deantoaicte/"+college+"/"+e, requestOptions)
+      .then(response => response.text())
+      .then((result)=>{ 
+        swal({
+          title: "Disapproved!",
+          text: "Project has been Disapproved!",
+          icon: "success",
+          button: "ok",
+        });
+      
+      })
+      .catch(error => console.log('error', error));
+
+
+
+  }
+    const approveDean=(e)=>{
+
+      fetch("http://localhost:8000/project/deantoaicte/"+college+"/"+e)
+  .then(response => response.text())
+  .then((result)=>{ 
+    swal({
+      title: "Approved!",
+      text: "Project has been Approved!",
+      icon: "success",
+      button: "ok",
+    });
+  
+  })
+  .catch(error => console.log('error', error));
+
+
+
+    }
+    const disapprovedDean=(e)=>{
+
+      var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+      };
+      
+      fetch("http://localhost:8000/project/hodtodean/"+college+"/"+e, requestOptions)
+        .then(response => response.text())
+        .then((result)=>{ 
+          swal({
+            title: "Disapproved!",
+            text: "Project has been Disapproved!",
+            icon: "success",
+            button: "ok",
+          });
+        
+        })
+        .catch(error => console.log('error', error));
+
+
+
+    }
+
+    const approveHod=(e)=>{
+
+      fetch("http://localhost:8000/project/hodtodean/"+college+"/"+e)
+  .then(response => response.text())
+  .then((result)=>{ 
+    swal({
+      title: "Approved!",
+      text: "Project has been Approved!",
+      icon: "success",
+      button: "ok",
+    });
+  
+  })
+  .catch(error => console.log('error', error));
+
+    }
+
+    const disapprovedHod=(e)=>{
+
+      var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+      };
+      
+      fetch("http://localhost:8000/project/guidtohod/"+college+"/"+e, requestOptions)
+        .then(response => response.text())
+        .then((result)=>{ 
+          swal({
+            title: "Disapproved!",
+            text: "Project has been Disapproved!",
+            icon: "success",
+            button: "ok",
+          });
+        
+        })
+        .catch(error => console.log('error', error));
+
+
+
+    }
+
+    const approveGuid=(e)=>{
+
+      fetch("http://localhost:8000/project/guidtohod/"+college+"/"+e)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  swal({
+    title: "Approved!",
+    text: "Project has been Approved!",
+    icon: "success",
+    button: "ok",
+  });
+    }
 
 
     const [con,setcon]=useState(false);
@@ -146,7 +325,7 @@ const Approval = () => {
         fetch("http://localhost:8000/project/")
           .then((response) => response.json())
           .then((data) =>{
-            data=data.filter((e)=>e.role==2) 
+            data=data.filter((e)=>e.guid!=null) 
           console.log(data);
           projectsetUser(data)});
 
@@ -277,7 +456,7 @@ return (
                         type="button"
                         className="btn  mb-3"
                         
-                        onClick={() => details(projectdata.id)}
+                        onClick={() => approveGuid(projectdata.id)}
                         style={{
                           width:"150px",
                           marginTop:"-50px",
@@ -382,7 +561,7 @@ return (
                         type="button"
                         className="btn  mb-3"
                         
-                        onClick={() => details(projectdata.id)}
+                        onClick={() => approveHod(projectdata.id)}
                         style={{
                           width:"150px",
                           marginTop:"-50px",
@@ -399,7 +578,7 @@ return (
                       <button
                         type="button"
                         className="btn  mb-3"
-                        onClick={() => deleteProject(projectdata.id)}
+                        onClick={() => disapprovedHod(projectdata.id)}
 
                         style={{
                           width:"150px",
@@ -420,7 +599,7 @@ return (
                         type="button"
                         className="btn  mb-3"
                         onClick={() => deleteProject(projectdata.id)}
-
+                        onDoubleClick
                         style={{
                           width:"150px",
                           marginTop:"-100px",
@@ -441,8 +620,220 @@ return (
 
     </div>
 }
-{dean1 && <Dean/>}
-{aict1  && <Aicte/>}
+{dean1 && 
+
+<div>
+{projectdean &&
+     projectdean.length > 0 &&
+     projectdean.map((projectdata) => (
+<div className="col-6 mt-5 ml-5" style={{ marginLeft: "350px" }}>
+
+         <div
+           className="card"
+           style={{ width: "50rem",height:"80%", marginLeft: "150xp" }}
+         ><Badge className="m-2" pill bg="secondary" style={{
+           height:"50px",
+           fontSize:"2em",
+
+         }}>DEAN</Badge>
+           <div className="card-body">
+             <p
+               className="card-title mb-5 mt-3"
+               style={{ marginLeft: "10px" }}
+             >
+               <a
+                 href={projectdata.Storage_link}
+                 style={{ textDecoration: "none",fontSize:"1.4rem" }}
+     >
+        <Badge className="m-2" pill bg="secondary"
+        style={{
+           position:"absolute",
+           right:"0%",
+           
+        }}
+        >
+                 {projectdata.type}
+               </Badge>
+   
+
+                 <h3>{projectdata.name} </h3>
+               </a>     
+            
+               <p style={{fontSize:"0.8rem"}}>Updated on {projectdata.end_date}</p>
+             <p>{projectdata.description}</p>
+             </p>
+             
+            {condean&& <div><button
+                   type="button"
+                   className="btn  mb-3"
+                   
+                   onClick={() => approveDean(projectdata.id)}
+                   style={{
+                     width:"150px",
+                     marginTop:"-50px",
+                     marginLeft:"0px",
+                     background:"green",
+                     borderColor:"green",
+                     color:"white",
+                     fontWeight:"bold"
+                   }}
+                   
+                 >
+                   Approve
+                 </button>
+                 <button
+                   type="button"
+                   className="btn  mb-3"
+                   onClick={() => disapprovedDean(projectdata.id)}
+
+                   style={{
+                     width:"150px",
+                     marginTop:"-50px",
+                     marginLeft:"25px",
+                     background:"red",
+                     borderColor:"red",
+                     color:"white",
+                     fontWeight:"bold"
+                   }}
+                  
+                 >
+                   Disapprove
+                 </button>
+                 </div>
+                 }
+                 <button
+                   type="button"
+                   className="btn  mb-3"
+                   onClick={() => deleteProject(projectdata.id)}
+
+                   style={{
+                     width:"150px",
+                     marginTop:"-100px",
+                     marginLeft:"350px",
+                     background:"#808080",
+                     borderColor:"#808080",
+                     color:"white",
+                     fontWeight:"bold"
+                   }}
+                  
+                 >
+                   Message
+                 </button>
+                 </div>
+         </div>
+       </div>
+))}
+
+</div>
+
+}
+{aict1 && 
+
+<div>
+{projectaict &&
+     projectaict.length > 0 &&
+     projectaict.map((projectdata) => (
+<div className="col-6 mt-5 ml-5" style={{ marginLeft: "350px" }}>
+
+         <div
+           className="card"
+           style={{ width: "50rem",height:"80%", marginLeft: "150xp" }}
+         ><Badge className="m-2" pill bg="secondary" style={{
+           height:"50px",
+           fontSize:"2em",
+
+         }}>DEAN</Badge>
+           <div className="card-body">
+             <p
+               className="card-title mb-5 mt-3"
+               style={{ marginLeft: "10px" }}
+             >
+               <a
+                 href={projectdata.Storage_link}
+                 style={{ textDecoration: "none",fontSize:"1.4rem" }}
+     >
+        <Badge className="m-2" pill bg="secondary"
+        style={{
+           position:"absolute",
+           right:"0%",
+           
+        }}
+        >
+                 {projectdata.type}
+               </Badge>
+   
+
+                 <h3>{projectdata.name} </h3>
+               </a>     
+            
+               <p style={{fontSize:"0.8rem"}}>Updated on {projectdata.end_date}</p>
+             <p>{projectdata.description}</p>
+             </p>
+             
+            {conaict&& <div><button
+                   type="button"
+                   className="btn  mb-3"
+                   
+                   onClick={() => approveAicte(projectdata.id)}
+                   style={{
+                     width:"150px",
+                     marginTop:"-50px",
+                     marginLeft:"0px",
+                     background:"green",
+                     borderColor:"green",
+                     color:"white",
+                     fontWeight:"bold"
+                   }}
+                   
+                 >
+                   Approve
+                 </button>
+                 <button
+                   type="button"
+                   className="btn  mb-3"
+                   onClick={() => disapprovedaict(projectdata.id)}
+
+                   style={{
+                     width:"150px",
+                     marginTop:"-50px",
+                     marginLeft:"25px",
+                     background:"red",
+                     borderColor:"red",
+                     color:"white",
+                     fontWeight:"bold"
+                   }}
+                  
+                 >
+                   Disapprove
+                 </button>
+                 </div>
+                 }
+                 <button
+                   type="button"
+                   className="btn  mb-3"
+                   onClick={() => deleteProject(projectdata.id)}
+
+                   style={{
+                     width:"150px",
+                     marginTop:"-100px",
+                     marginLeft:"350px",
+                     background:"#808080",
+                     borderColor:"#808080",
+                     color:"white",
+                     fontWeight:"bold"
+                   }}
+                  
+                 >
+                   Message
+                 </button>
+                 </div>
+         </div>
+       </div>
+))}
+
+</div>
+
+}
 
     </>
 )
